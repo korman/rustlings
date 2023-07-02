b0fe95bac464c63341d0b1cb9facb464e1b94859
Korman <korman@qq.com>
2023-07-02 23:07:42 +0800

    更新

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,8 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
@@ -30,4 +28,16 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(value: i32) -> i32 {
+    let mut price = 0;
+
+    if value > 40 {
+        price = value;
+    } else {
+        price = value * 2;
+    }
+
+    return price;
 }
